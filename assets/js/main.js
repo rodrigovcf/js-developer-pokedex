613676b8e8faebd8c85a8d9e9950ab1f860137e2
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2022-12-28 10:58:53 -0300

    Pokemon Selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonDetails = document.getElementById('pokemonDetails')
+
+pokes = []
 
 const maxRecords = 151
 const limit = 10
@@ -9,7 +12,7 @@ function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <span class="name"><a href="javascript:getPokemonDetails(${pokemon.number})">${pokemon.name}</a></span>
 
             <div class="detail">
                 <ol class="types">
@@ -22,6 +25,36 @@ function convertPokemonToLi(pokemon) {
         </li>
     `
 }
+
+function getPokemonDetails(number){         
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+    pokemonList.parentElement.removeChild(pokemonList)
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokes = pokemons.map(convertPokemon)
+        pokemonDetails.innerHTML = pokes[number]
+    })
+
+}
+
+function convertPokemon(pokemon) {
+    return `
+        <div class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+            <a href="index.html" type="button">Voltar</a>
+        </div>
+    `
+}
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
